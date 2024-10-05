75441552bffb4a962ff945cd4bffc4a7ce48366b
whatIsLove <vlad.galyara@gmail.com>
2024-10-05 17:51:26 +0300

    init commit

---
+++ accounts/src/main/java/com/cr1stal423/accounts/AccountsApplication.java
@@ -0,0 +1,13 @@
+package com.cr1stal423.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ accounts/src/test/java/com/cr1stal423/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cr1stal423.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
