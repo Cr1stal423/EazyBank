9c0ca07a259b36a51b43ac4a134e7dd826e286fd
whatIsLove <vlad.galyara@gmail.com>
2024-10-11 17:47:56 +0300

    fix(cards): fix bug with cardId

--- cards/src/main/java/com/cr1stal423/cards/entity/Cards.java
+++ cards/src/main/java/com/cr1stal423/cards/entity/Cards.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 public class Cards extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long carId;
+    private Long cardId;
 
     private String mobileNumber;
 
