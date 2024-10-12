12439ff061371390a686d31eda3d29ac0eb0ef4a
whatIsLove <vlad.galyara@gmail.com>
2024-10-12 20:18:10 +0300

    feat(feign-client): add feign client related changes

--- accounts/src/main/java/com/cr1stal423/accounts/mapper/CustomerMapper.java
+++ accounts/src/main/java/com/cr1stal423/accounts/mapper/CustomerMapper.java
@@ -1,5 +1,6 @@
 package com.cr1stal423.accounts.mapper;
 
+import com.cr1stal423.accounts.dto.CustomerDetailsDto;
 import com.cr1stal423.accounts.dto.CustomerDto;
 import com.cr1stal423.accounts.entity.Customer;
 
@@ -16,5 +17,11 @@ public class CustomerMapper {
         customer.setEmail(customerDto.getEmail());
         customer.setMobileNumber(customerDto.getMobileNumber());
         return customer;
+    }
+    public static CustomerDetailsDto mapToCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDto){
+        customerDetailsDto.setName(customer.getName());
+        customerDetailsDto.setEmail(customer.getEmail());
+        customerDetailsDto.setMobileNumber(customer.getMobileNumber());
+        return customerDetailsDto;
     }
 }
