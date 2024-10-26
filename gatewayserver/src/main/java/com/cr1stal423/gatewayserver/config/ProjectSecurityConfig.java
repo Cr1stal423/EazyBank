f1928319530e7f34ac3790ff42835a20b117b4dc
whatIsLove <vlad.galyara@gmail.com>
2024-10-26 17:39:39 +0300

    feat(gatewayserver): implement functionality for converting Keycloak roles

--- gatewayserver/src/main/java/com/cr1stal423/gatewayserver/config/ProjectSecurityConfig.java
+++ gatewayserver/src/main/java/com/cr1stal423/gatewayserver/config/ProjectSecurityConfig.java
@@ -2,11 +2,18 @@ package com.cr1stal423.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,13 +22,17 @@ public class ProjectSecurityConfig {
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
         http.authorizeExchange(exhanges -> exhanges
                         .pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/eazybank/accounts/**").authenticated()
-                        .pathMatchers("/eazybank/cards/**").authenticated()
-                        .pathMatchers("/eazybank/loans/**").authenticated())
+                        .pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/eazybank/cards/**").hasRole("CARDS")
+                        .pathMatchers("/eazybank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults()));
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         http.csrf(csrf -> csrf.disable());
         return http.build();
-
+    }
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
